c83d9597d2203d22f9f19efa89f07e49ee54a243
AraHaan <seandhunt_7@yahoo.com>
2017-06-03 16:45:02 -0500

    Attempt to fix setup.py.

--- setup.py
+++ setup.py
@@ -2,28 +2,45 @@ from setuptools import setup
 from setuptools.extension import Extension
 import sys
 
-requirements = []
-try:
-    with open('requirements.txt') as f:
-        requirements = f.read().splitlines()
-except Exception as ex:
-    with open('DecoraterBotUtils.egg-info\requires.txt') as f:
-        requirements = f.read().splitlines()
 
-version = '0.0.1'
-
-if sys.platform != 'cygwin':
-    BotErrors = Extension(
-        'DecoraterBotUtils.BotErrors', [
-            'DecoraterBotUtils/BotErrors.c'])
-else:
-    BotErrors = Extension(
-        'DecoraterBotUtils.BotErrors',
-        library_dirs=['/usr/local/bin'],
-        sources=['DecoraterBotUtils/BotErrors.c'])
+def get_requirements():
+    """
+    lists the requirements to install.
+    """
+    requirements = []
+    try:
+        with open('requirements.txt') as f:
+            requirements = f.read().splitlines()
+    except Exception as ex:
+        with open('DecoraterBotUtils.egg-info\requires.txt') as f:
+            requirements = f.read().splitlines()
+    return requirements
 
 
-if not version:
+def get_version():
+    """
+    returns version.
+    """
+    return '0.0.1'
+
+
+def get_extensions():
+    """
+    lists the extensions to build with an compiler.
+    """
+    if sys.platform != 'cygwin':
+        BotErrors = Extension(
+            'DecoraterBotUtils.BotErrors', [
+                'DecoraterBotUtils/BotErrors.c'])
+    else:
+        BotErrors = Extension(
+            'DecoraterBotUtils.BotErrors',
+            library_dirs=['/usr/local/bin'],
+            sources=['DecoraterBotUtils/BotErrors.c'])
+    return [BotErrors]
+
+
+if not get_version():
     raise RuntimeError('version is not set')
 
 try:
@@ -37,16 +54,16 @@ setup(name='DecoraterBotUtils',
       author_email='seandhunt_7@yahoo.com',
       url='https://github.com/DecoraterBot-devs/DecoraterBotUtils',
       bugtrack_url='https://github.com/DecoraterBot-devs/DecoraterBotUtils/issues',
-      version=version,
+      version=get_version(),
       packages=['DecoraterBotUtils'],
-      ext_modules=[BotErrors],
+      ext_modules=get_extensions(),
       license='MIT',
       description='This package is for bringing various things to DecoraterBot.',
       long_description=readme,
       maintainer_email='seandhunt_7@yahoo.com',
       download_url='https://github.com/DecoraterBot-devs/DecoraterBotUtils',
       include_package_data=True,
-      install_requires=requirements,
+      install_requires=get_requirements(),
       platforms='Any',
       classifiers=[
         'Development Status :: 5 - Production/Stable',
