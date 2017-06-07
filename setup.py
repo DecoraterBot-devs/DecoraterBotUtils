00e7881fa9bb9d998bf1142c33ab8dd7302ca529
AraHaan <seandhunt_7@yahoo.com>
2017-06-06 23:40:17 -0500

    Fixed installation.

--- setup.py
+++ setup.py
@@ -16,15 +16,11 @@ def get_requirements(remove_links=True):
             requirements = f.read().splitlines()
     if remove_links:
         for requirement in requirements:
-        # git repository url.
-        if requirement.startswith("git+"):
-            requirements.remove(requirement)
-        # subversion repository url.
-        if requirement.startswith("svn+"):
-            requirements.remove(requirement)
-        # mercurial repository url.
-        if requirement.startswith("hg+"):
-            requirements.remove(requirement)
+            # git/subversion/mercurial repository url.
+            if requirement.startswith("git+") or \
+                requirement.startswith("svn+") or \
+                requirement.startswith("hg+"):
+                requirements.remove(requirement)
     return requirements
 
 
@@ -34,23 +30,11 @@ def get_links():
     """
     links_list = get_requirements(remove_links=False)
     for link in links_list:
-        keep_link = False
-        already_removed = False
-        # git repository url.
-        if not link.startswith("git+"):
-            if not link.startswith("svn+"):
-                if not link.startswith("hg+"):
-                    links_list.remove(link)
-                    already_removed = True
-                else:
-                    keep_link = True
-                if not keep_link and not already_removed:
-                    links_list.remove(link)
-                    already_removed = True
-            else:
-                keep_link = True
-            if not keep_link and not already_removed:
-                links_list.remove(link)
+        # git/subversion/mercurial repository url.
+        if not link.startswith("git+") or not \
+            link.startswith("svn+") or not \
+            link.startswith("hg+"):
+            links_list.remove(link)
     return links_list
 
 
