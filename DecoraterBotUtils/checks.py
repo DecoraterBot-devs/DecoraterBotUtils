6a4ce512d47d4b612663a8c9b5e4e453a00ded9e
AraHaan <seandhunt_7@yahoo.com>
2022-11-30 10:55:34 -0500

    Fixed bot owner check.

--- DecoraterBotUtils/checks.py
+++ DecoraterBotUtils/checks.py
@@ -10,5 +10,5 @@ class Checks:
     @staticmethod
     def is_bot_owner():
         def predicate(interaction: discord.Interaction) -> bool:
-            return interaction.user.id == int(interaction.client.BotConfig.discord_user_id)
+            return interaction.client.is_owner(interaction.user.id)
         return app_commands.check(predicate)
