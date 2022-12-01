f4b1346c647eec6324ae689cad0fbba2ed917450
AraHaan <seandhunt_7@yahoo.com>
2022-11-30 23:51:52 -0500

    Changes to help reduce possible blocking issues on the async loop.

--- DecoraterBotUtils/client.py
+++ DecoraterBotUtils/client.py
@@ -25,7 +25,7 @@ class BotClient(commands.Bot):
     This is where the Events are Registered.
     """
     def __init__(self, **kwargs):
-        self._start = time.time()
+        self.uptime_count_begin = time.time()
         self.logged_in_ = False
         self.somebool = False
         self.reload_normal_commands = False
@@ -35,80 +35,22 @@ class BotClient(commands.Bot):
         self.rejoin_after_reload = False
         self.sent_prune_error_message = False
         self.is_bot_logged_in = False
+        self.consoletext = BaseConfigReader(file='ConsoleWindow.json')[config.language]
         super(BotClient, self).__init__(
             command_prefix=config.bot_prefix,
             status=discord.Status.online,
             activity=discord.Streaming(
                 name=self.consoletext['On_Ready_Game'][0],
-                url=self.BotConfig.twitch_url),
+                url=config.twitch_url),
             # intents=self.bot_intents,
             intents=discord.Intents.default(),
             **kwargs)
+        self.stdout = open(os.path.join(sys.path[0], 'resources', 'Logs', 'console.log'), 'w')
         self.tree.on_error = self.on_app_command_error
         self.call_all()
 
     async def setup_hook(self) -> None:
         await self.load_all_default_plugins()
-
-    @property
-    def version(self):
-        """
-        returns the bot's version number.
-        """
-        return self.consoletext['WindowVersion'][0]
-
-    @property
-    def BotConfig(self):
-        """
-        Reads the bot's config data.
-        """
-        type(self)
-        return config
-
-    @property
-    def consoletext(self):
-        """
-        returns the bot's
-        console text.
-        """
-        consoledata = BaseConfigReader(file='ConsoleWindow.json').config
-        consoledata = consoledata[self.BotConfig.language]
-        return consoledata
-
-    @property
-    def commands_list(self):
-        """
-        retrieves a list of all the bot's registered commands.
-        """
-        plugin_list = []
-        for command in self.commands:
-            plugin_list.append(command)
-        return plugin_list
-
-    @property
-    def uptime_count_begin(self):
-        """
-        returns the start time.
-        """
-        return self._start
-
-    @property
-    def credits(self):
-        """
-        returns the stuff that the Credits reader returns.
-        """
-        type(self)
-        return PluginConfigReader(file="credits.json", credits=True)
-
-    @property
-    def credentials_check(self):
-        """
-        Checks if the Credentials.json
-        file exists.
-        """
-        PATH = os.path.join(
-            sys.path[0], 'resources', 'ConfigData', 'Credentials.json')
-        return os.path.isfile(PATH) and os.access(PATH, os.R_OK)
 
     async def load_all_default_plugins(self):
         """
@@ -116,7 +58,7 @@ class BotClient(commands.Bot):
         used to load up.
         """
         self.remove_command("help")
-        for plugins_cog in self.BotConfig.default_plugins:
+        for plugins_cog in config.default_plugins:
             ret = await self.load_plugin(plugins_cog)
             if isinstance(ret, str):
                 print(ret)
@@ -159,22 +101,17 @@ class BotClient(commands.Bot):
         if err is not None:
             return err
 
-    def changewindowtitle(self):
-        """
-        Changes the console's window Title.
-        """
-        consolechange.consoletitle(
-            self.consoletext['WindowName'][0] + self.version)
-
     def call_all(self):
         """
         calls all functions that __init__ used to
         call except for super.
         """
+        handler = logging.StreamHandler(stream=self.stdout)
         formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
         level = logging.INFO
-        discord.utils.setup_logging(formatter=formatter, level=level)
-        self.changewindowtitle()
+        discord.utils.setup_logging(handler=handler, formatter=formatter, level=level)
+        consolechange.consoletitle(
+            f'{self.consoletext["WindowName"][0]}{self.consoletext["WindowVersion"][0]}')
 
     async def login_helper(self):
         """
@@ -187,21 +124,12 @@ class BotClient(commands.Bot):
         Allows the bot to Connect / Reconnect.
         :return: Nothing or -1/-2 on failure.
         """
-        _continue = False
-        if self.BotConfig.config is None:
-            if self.credentials_check:
-                _continue = True
-            else:
-                print(str(self.consoletext['Credentials_Not_Found'][0]))
-                return
-        else:
-            _continue = True
+        _continue = True if config.config is not None and config.found is True else False
         if _continue:
             try:
-                if self.BotConfig.bot_token is not None:
+                if config.bot_token is not None:
                     self.is_bot_logged_in = True
-                    await self.start(
-                        self.BotConfig.bot_token)
+                    await self.start(config.bot_token)
             except discord.errors.GatewayNotFound:
                 print(str(self.consoletext['Login_Gateway_No_Find'][0]))
                 return
@@ -218,6 +146,9 @@ class BotClient(commands.Bot):
                 pass
             except Exception:
                 pass
+        else:
+            print(str(self.consoletext['Credentials_Not_Found'][0]))
+            return
 
     async def on_app_command_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
         """..."""
@@ -230,57 +161,13 @@ class BotClient(commands.Bot):
         else:
             pass
 
-    def on_bot_error(self, funcname, tbinfo, err):
-        """
-        This Function is for Internal Bot use only.
-        It is for catching any Errors and writing them to a file.
-
-        Usage
-        =====
-        :param funcname: Must be a string with the name of the function
-        that caused an Error.
-            raises the Errors that happened if empty string or None is
-            given.
-        :param tbinfo: string data of the traceback info. Must be a
-            string for this to not Error itself.
-            raises the Errors that happened if empty string or None is
-            given.
-        :param err: Error Data from Traceback. (RAW)
-        """
-        str(self)
-        if bool(funcname):
-            if bool(tbinfo):
-                exception_data = 'Ignoring exception caused at {0}:\n' \
-                                 '{1}'.format(funcname, tbinfo)
-                logfile = os.path.join(
-                    sys.path[0], 'resources', 'Logs', 'error_log.log')
-                try:
-                    file = open(logfile, 'a', encoding='utf-8')
-                    size = os.path.getsize(logfile)
-                    if size >= 32102400:
-                        file.seek(0)
-                        file.truncate()
-                    file.write(exception_data)
-                    file.close()
-                except PermissionError:
-                    return
-            else:
-                raise err
-        else:
-            raise err
-
     async def on_ready(self):
         """
         Bot Event.
         :return: Nothing.
         """
         await self.tree.sync()
-        try:
-            await self.on_bot_login()
-        except Exception as e:
-            funcname = 'on_ready'
-            tbinfo = str(traceback.format_exc())
-            self.on_bot_error(funcname, tbinfo, e)
+        await self.on_bot_login()
 
     async def on_bot_login(self):
         """
@@ -293,26 +180,10 @@ class BotClient(commands.Bot):
             print(Fore.GREEN + Back.BLACK + Style.BRIGHT + str(
                 self.consoletext['Window_Login_Text'][0]).format(
                 self.user.name, self.user.id, discord.__version__))
-            sys.stdout = open(os.path.join(
-                sys.path[0], 'resources', 'Logs', 'console.log'),
-                'w')
+            sys.stdout = self.stdout
             sys.stderr = open(os.path.join(
                 sys.path[0], 'resources', 'Logs',
                 'unhandled_tracebacks.log'), 'w')
-
-    async def on_error(self, event, *args, **kwargs):
-        """
-        Bot Event.
-        :param event: Event.
-        :param args: Args.
-        :param kwargs: Other Args.
-        :return: Nothing.
-        """
-        str(args)
-        str(kwargs)
-        funcname = event
-        tbinfo = str(traceback.format_exc())
-        self.on_bot_error(funcname, tbinfo, None)
 
     # Helpers.
     async def resolve_send_message_error(self, interaction: discord.Interaction):
@@ -326,16 +197,6 @@ class BotClient(commands.Bot):
                     interaction.channel.name))
         except discord.errors.Forbidden:
             return
-
-    def command_traceback_helper(self, tbinfo):
-        """
-        Helps iterate through a list of every
-        line in a command's traceback.
-        """
-        tracebackdata = ""
-        for line in tbinfo:
-            tracebackdata = tracebackdata + line
-        return tracebackdata
 
     # def bot_intents(self) -> discord.Intents:
     #     _intents = discord.Intents.default()
--- DecoraterBotUtils/readers.py
+++ DecoraterBotUtils/readers.py
@@ -6,9 +6,7 @@ import json
 import os
 import sys
 
-__all__ = [
-    'BaseConfigReader', 'PluginConfigReader',
-    'BotCredentialsReader']
+__all__ = ['BaseConfigReader', 'BotCredentialsReader']
 
 
 class BaseConfigReader:
@@ -18,83 +16,41 @@ class BaseConfigReader:
     def __init__(self, file=None):
         self.config = None
         self.filename = file
-        self.file = None
-        self.json_file = None
+        self.found = False
+        self.json_file = os.path.join(sys.path[0], 'resources', 'ConfigData', self.filename)
         self.load()
+
+    def __getitem__(self, item):
+        """
+        Gets a JSON Config Value basted on the key provided.
+        :param item: String key to the entry in the JSON file
+        :return: JSON config Value.
+        """
+        return self.config[item]
+
+    def __setitem__(self, key, value):
+        """
+        Sets a JSON Config Value basted on the key and data provided.
+        :param key: String key to the entry in the JSON file
+        :param value: Value to replace old value with.
+        """
+        self.config[key] = value
 
     def load(self):
         """
         Loads the JSON config Data.
         :return: List.
         """
-        self.json_file = os.path.join(
-            sys.path[0], 'resources', 'ConfigData',
-            self.filename)
+        self.found = os.path.isfile(self.json_file) and os.access(self.json_file, os.R_OK)
         try:
-            with open(self.json_file) as self.file:
-                self.config = json.load(self.file)
+            with open(self.json_file) as file:
+                self.config = json.load(file)
         except(OSError, IOError):
             pass
 
-    def getconfig(self, key):
-        """
-        Gets a JSON Config Value basted on the key provided.
-        :param key: String key to the entry in the JSON file
-        :return: JSON config Value.
-        """
-        return self.config[key]
-
-    def setconfig(self, key, data):
-        """
-        Sets a JSON Config Value basted on the key and data provided.
-        :param key: String key to the entry in the JSON file
-        :param data: Data to replace old value with.
-        """
-        self.config[key] = data
-        with open(self.json_file) as self.file:
-            json.dump(self.config, self.file, indent=4, sort_keys=True)
-
-
-class PluginConfigReader(BaseConfigReader):
-    """
-    Class for getting plugin config values.
-    """
-    def __init__(self, file=None, credits=False):
-        self.credits = credits
-        super(PluginConfigReader, self).__init__(file=file)
-
-    def getcredits(self, key, key2):
-        """
-        Gets a JSON Config Value basted on the key provided.
-        :param key: String key to the entry in the JSON file.
-        :param key2: String key to the entry in the JSON file.
-        :return: JSON config Value.
-        """
-        if not self.credits:
-            return None
-
-        return self.config[key][key2]
-
-    def setcredits(self, key, key2, data):
-        """
-        Sets a JSON Config Value basted on the key and data provided.
-        :param key: String key to the entry in the JSON file
-        :param key2: String key to the entry in the JSON file.
-        :param data: Data to replace old value with.
-        """
-        if not self.credits:
-            return None
-
-        try:
-            self.config[key][key2] = data
-        except (KeyError, TypeError):
-            self.config[key] = {}
-            self.config[key][key2] = data
-        try:
-            with open(self.json_file, mode='w') as file:
-                file.write(json.dumps(self.config, indent=4, sort_keys=True))
-        except(OSError, IOError):
-            pass
+    def save(self):
+        with open(self.json_file, mode='w') as file:
+            json.dump(self.config, file, indent=4, sort_keys=True)
 
 
 class BotCredentialsReader(BaseConfigReader):
@@ -108,14 +64,14 @@ class BotCredentialsReader(BaseConfigReader):
         self.log_error = False  # bool
         self.pm_command_errors = False  # bool
         self.enable_error_handler = False  # bool
-        self.bot_prefix = ''  # string
-        self.bot_token = ''  # string
-        self.language = 'en'  # string
-        self.description = ''  # string
-        self.twitch_url = ''  # string
-        self.default_plugins = []  # list
 
         # populate the values from Credentials.json.
+        self.bot_prefix = self['bot_prefix']  # string
+        self.bot_token = self['token']  # string
+        self.language = self['language']  # string
+        self.description = self['description']  # string
+        self.twitch_url = self['twitch_url']  # string
+        self.default_plugins = self['default_plugins']  # dict
         self.set_values()
 
     def set_values(self):
@@ -123,39 +79,12 @@ class BotCredentialsReader(BaseConfigReader):
         sets values of the variables.
         """
         try:
-            self.log_error = self.getconfig(
-                'log_error')  # bool
+            self.log_error = self['log_error']  # bool
         except (KeyError, TypeError):
             pass
         try:
-            self.pm_command_errors = self.getconfig(
-                'pm_command_errors')  # bool
+            self.pm_command_errors = self['pm_command_errors']  # bool
         except (KeyError, TypeError):
             pass
         self.enable_error_handler = (
             True if not self.pm_command_errors else False)  # bool
-        try:
-            self.bot_token = self.getconfig(
-                'token')  # string
-        except (KeyError, TypeError):
-            pass
-        try:
-            self.language = self.getconfig(
-                'language')  # string
-        except (KeyError, TypeError):
-            pass
-        try:
-            self.description = self.getconfig(
-                'description')  # string
-        except (KeyError, TypeError):
-            pass
-        try:
-            self.twitch_url = self.getconfig(
-                'twitch_url')  # string
-        except (KeyError, TypeError):
-            pass
-        try:
-            self.default_plugins = self.getconfig(
-                'default_plugins')  # dict
-        except (KeyError, TypeError):
-            pass
